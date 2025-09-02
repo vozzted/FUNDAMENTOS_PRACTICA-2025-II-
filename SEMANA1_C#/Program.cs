952afaa2e710278275bb07a815b4f3ccfd3031e4
victor <victor@gmail.com>
2025-09-02 15:29:57 -0500

    subiendo ejercicio1-SEM1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, carrera;
+
+            Console.Write("cual es tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("cual es tu carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine( nombre + ", Bienvenido al curso de fundamentos de algoritmos de la carrera " + carrera);
+            Console.ReadKey();
+        }
+        static void ejer1()
+        { 
+        
+        
+        }
+        static void ejer2()
+        {
+
+
+        }
+        static void ejer3()
+        {
+
+
+        }
+
+    }
+}
---
+++ SEMANA1_PYTHON/SEMANA1_PYTHON.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre = input ("Ingrese su nombre: ")
+    carrera = input ("Ingrese su carrera: ")
+    print(f"{nombre}, bienvenido a FA de {carrera}")
+
+ejer1()
+     
